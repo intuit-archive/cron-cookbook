5dad86db694e8ffcad60c3cb78c7c066f0c2823b
Tommy Bishop <bishop.thomas@gmail.com>
2013-07-10 13:00:19 -0700

    put version back down after testing

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chad_cloes@intuit.com'
 license          'Apache 2.0'
 description      'Configures cron'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.8.0'
+version          '0.1.0'
 
 %w{redhat centos}.each do |os|
   supports os
