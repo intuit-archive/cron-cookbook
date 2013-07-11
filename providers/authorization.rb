4becc2eaa8fb300be71338f83113e4603d5510ab
Dylan Vaughn <dylancvaughn@gmail.com>
2013-07-11 16:31:16 -0700

    updates based on peer review

--- providers/authorization.rb
+++ providers/authorization.rb
@@ -1,17 +1,11 @@
 action :create do
   name = new_resource.name
 
-  file "/etc/cron.allow" do
-    owner "root"
-    group "root"
-    mode "0644"
-    action :create_if_missing
-  end
-
   execute "add #{name} to /etc/cron.allow" do
     command "echo #{name} >> /etc/cron.allow"
     not_if do
-      IO.read('/etc/cron.allow').include? "\n#{name}\n"
+      ::File.file?('/etc/cron.allow') &&
+        IO.read('/etc/cron.allow').include?("\n#{name}\n")
     end
   end
 
--- test/integration/default/minitest/test_test.rb
+++ test/integration/default/minitest/test_test.rb
@@ -3,7 +3,6 @@ require 'minitest/autorun'
 describe "cron::test" do
   
   it "creates the cron.allow file and adds to it" do
-    assert File.exists?('/etc/cron.allow')
     assert IO.read('/etc/cron.allow').include? "testuser"
   end
 
