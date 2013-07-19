0b18de47bea3ed8ac3dbdc531f385ca6c2cad528
Dylan Vaughn <dylancvaughn@gmail.com>
2013-07-19 10:42:47 -0700

    version bump to 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chad_cloes@intuit.com'
 license          'Apache 2.0'
 description      'Configures cron'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 %w{redhat centos}.each do |os|
   supports os
